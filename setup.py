e9935d95e273d1285f0a0d90a1a83bdd06255f32
Luca Fabbri <luca@keul.it>
2014-02-19 16:16:48 +0100

    Preparing release 1.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ This module contains the tool of rt.calendarinandout
 import os
 from setuptools import setup, find_packages
 
-version = '1.2.0.dev0'
+version = '1.2.0'
 
 setup(name='rt.calendarinandout',
       version=version,
